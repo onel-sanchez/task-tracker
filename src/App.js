9a8f94f16d559999d0132e68ef12c78034832562
onel-sanchez <60905558+onel-sanchez@users.noreply.github.com>
2021-05-23 16:17:00 -0400

    Reminder and Form

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import { useState } from 'react'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
-      {
-        id: 1,
-        text: 'Doctor Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'Feb 7th at 3:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food shopping',
-        day: 'Feb 7th at 5:30pm',
-        reminder: false,
-      },
-     ]);
+    {
+      id: 1,
+      text: "Doctor Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 7th at 3:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food shopping",
+      day: "Feb 7th at 5:30pm",
+      reminder: false,
+    },
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
-      <Tasks tasks={tasks} />  
+      <AddTask />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Task to show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,17 @@
-import Task from './Task'
+import Task from "./Task";
 
-const Tasks = ({ tasks }) => {
-    return (
-        <>
-            {tasks.map((task) => (
-                <Task key={task.id} task={task}/>
-            ))}
-        </>
-    )
-}
-export default Tasks
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </>
+  );
+};
+export default Tasks;
